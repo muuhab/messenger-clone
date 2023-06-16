f68fb47225c2decf886f8c23814c331cf2614a0a
Mohab Sherif <mohab.barca@gmail.com>
2023-06-16 14:42:16 +0300

    Added assets and configs

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    swcPlugins: [["next-superjson-plugin", {}]],
+  },
+  images: {
+    // This is a list of domains that are allowed to be used for
+    // Image Optimization. The default is just your domain name.
 
-module.exports = nextConfig
+    domains: [
+      "res.cloudinary.com",
+      "avatars.githubusercontent.com",
+      "lh3.googleusercontent.com",
+    ],
+  },
+};
+
+module.exports = nextConfig;

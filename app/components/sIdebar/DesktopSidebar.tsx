61198dc605a002cc642811fd2fc30cba835a52b0
Mohab Sherif <mohab.barca@gmail.com>
2023-07-06 19:59:15 +0300

    Added profile and settings drawer

--- app/components/sIdebar/DesktopSidebar.tsx
+++ app/components/sIdebar/DesktopSidebar.tsx
@@ -5,6 +5,7 @@ import { useState } from "react"
 import DesktopItem from "./DesktopItem"
 import { User } from "@prisma/client"
 import Avatar from "../Avatar"
+import SettingModal from "./SettingModal"
 
 interface DesktopSidebarProps {
     currentUser: User
@@ -16,36 +17,43 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({
     const routes = useRoutes()
     const [isOpen, setIsOpen] = useState(false)
     return (
-        <div
-            className="hidden lg:fixed lg:inset-0 lg:left-0 lg:w-20 lg:z-40 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between"
-        >
-            <nav className="mt-4 flex flex-col justify-between">
-                <ul role="list"
-                    className="flex flex-col items-center space-y-1"
-                >
-                    {routes.map((route) => (
-                        <DesktopItem
-                            key={route.label}
-                            href={route.href}
-                            icon={route.icon}
-                            label={route.label}
-                            active={route.active}
-                            onClick={route.onClick}
+        <>
+            <SettingModal
+                currentUser={currentUser}
+                isOpen={isOpen}
+                onClose={() => setIsOpen(false)}
+            />
+            <div
+                className="hidden lg:fixed lg:inset-0 lg:left-0 lg:w-20 lg:z-40 xl:px-6 lg:overflow-y-auto lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between"
+            >
+                <nav className="mt-4 flex flex-col justify-between">
+                    <ul role="list"
+                        className="flex flex-col items-center space-y-1"
+                    >
+                        {routes.map((route) => (
+                            <DesktopItem
+                                key={route.label}
+                                href={route.href}
+                                icon={route.icon}
+                                label={route.label}
+                                active={route.active}
+                                onClick={route.onClick}
+                            />
+                        ))}
+                    </ul>
+                </nav>
+                <nav className="mt-4 flex flex-col justify-between items-center">
+                    <div onClick={() => setIsOpen(true)}
+                        className="cursor-pointer hover:opacity-75 transition"
+                    >
+                        <Avatar
+                            user={currentUser}
                         />
-                    ))}
-                </ul>
-            </nav>
-            <nav className="mt-4 flex flex-col justify-between items-center">
-                <div onClick={() => setIsOpen(true)}
-                    className="cursor-pointer hover:opacity-75 transition"
-                >
-                    <Avatar
-                        user={currentUser}
-                    />
-                </div>
-            </nav>
+                    </div>
+                </nav>
 
-        </div>
+            </div>
+        </>
     )
 }
 export default DesktopSidebar
